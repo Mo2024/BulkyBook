a417791b4fc523920d752dbfde3527e80092cff1
Mo2024 <mohdosama2025@gmail.com>
2023-05-11 20:39:53 +0300

    Partials created

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -41,6 +41,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -77,6 +78,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -119,6 +121,7 @@ namespace BulkyBookWeb.Controllers
 
             _db.Categories.Remove(category);
             _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
             return RedirectToAction("Index");
 
         }
