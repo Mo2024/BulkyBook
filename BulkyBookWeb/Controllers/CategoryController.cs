5c7a8f99cbfdc778e25a5dae4ab54f9f189ff1cb
Mo2024 <mohdosama2025@gmail.com>
2023-05-11 18:08:53 +0300

    Vallidation checks

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -31,9 +31,20 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The Display Order cannot exactly match the Name.");
+                //ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+      
         }
     }
 }
